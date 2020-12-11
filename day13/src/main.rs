de257cce3a9ac1f47a13713d7b121595bbf49e4d
Predrag Gruevski <obi1kenobi82@gmail.com>
2020-12-11 00:34:27 -0500

    Add stub for day13.

---
+++ day13/src/main.rs
@@ -0,0 +1,16 @@
+use std::fs;
+
+fn main() {
+    let contents = fs::read_to_string(
+        "/mnt/c/Users/predrag/Dropbox/Documents/Code/advent-of-code-2020/day13/sample_input.txt",
+    )
+    .unwrap();
+
+    let lines: Vec<_> = contents.trim().split("\n").collect();
+
+    println!("{}", solve_part1(&lines));
+}
+
+fn solve_part1(lines: &Vec<&str>) -> i32 {
+    0
+}
